7deefdc24e3cabc9c5ad78daa4649095e1548d69
Ntlele <ntleledavid07@gmail.com>
2024-01-30 17:12:20 -0500

    variuos updates

---
+++ leaderboard.js
@@ -0,0 +1,24 @@
+
+function getCookie(cname) {
+
+    let name = cname + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(';');
+  
+    for(let i = 0; i < ca.length; i++) {
+      let c = ca[i];
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return "";
+  }
+  
+  let user = getCookie("username");
+
+  //****************Getting the user score to update the leader board******* */
+
+const userscore = scoreup;
